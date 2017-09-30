b14739cfee2865934dd5e622f48a265b09deca2b
isidoris <valentin.sorlin@hotmail.com>
2017-09-30 06:45:41 +0200

    init project

---
+++ Assets/player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal")*0.1f;
+        var z = Input.GetAxis("Vertical")*0.1f;
+
+        transform.Translate(x, 0, z);
+    }
+}
