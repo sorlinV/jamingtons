82dd83c58b2136bbe049be42d49136a780b9d443
isidoris <valentin.sorlin@hotmail.com>
2017-09-30 15:17:11 +0200

    create bullet mouvement player ok

--- Assets/Script/player.cs
+++ Assets/Script/player.cs
@@ -7,8 +7,9 @@ public delegate void Funct();
 public class player : MonoBehaviour {
 	public GameObject bullet;
 	public float speed = 10;
+	public bool is_falling = true;
 	float delayed_shoot = 0;
-
+	
 	IEnumerator setTimeout(Funct callback, float time) {
         yield return new WaitForSeconds(time);
 		callback();
@@ -22,15 +23,22 @@ public class player : MonoBehaviour {
 				Quaternion dir = new Quaternion(transform.GetChild(0).rotation.x,
 					transform.GetChild(0).rotation.y + Random.Range(-spray, spray),
 					transform.GetChild(0).rotation.z, 0);
-				Instantiate(bullet, transform.GetChild(0).position, dir);
+				GameObject clone = Instantiate(bullet, transform.GetChild(0).position, dir) as GameObject;
+				clone.GetComponent<Rigidbody>().velocity = clone.transform.forward * (speed * 2);
 			}
 		}
 	}
 
 	void mouv()
 	{
-		Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-		transform.gameObject.GetComponent<Rigidbody>().velocity = (dir) * speed * Time.deltaTime;
+		Vector3 dir = transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal");
+		if (is_falling == true)
+		{
+			dir = transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal") - transform.up;
+		}
+		if (dir.x != 0 || dir.y != 0 || dir.z != 0) {
+			transform.gameObject.GetComponent<Rigidbody>().velocity = dir * speed;
+		}
 		transform.Rotate(new Vector3(0, Input.GetAxis("Mouse ScrollWheel") * speed, 0));
 	}
 
